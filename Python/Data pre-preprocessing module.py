6e60f638369bd39214476a0265af98e9c109289b
QV-137 <115896668+QuantVortex137@users.noreply.github.com>
2023-03-27 12:13:18 -0600

    Update and rename Part 1 - Data preprocessing.py to Data pre-preprocessing module.py

--- Python/Data pre-preprocessing module.py
+++ Python/Data pre-preprocessing module.py
@@ -1,26 +1,16 @@
-# -*- coding: utf-8 -*-
 """
-Técnicas de Pre-procesamiento de Datos___________________________________
-Para Machine Learning, antes de escribir cualquier modelo
-de aprendizaje se tienen que procesar todos los datos que se
-usarán a lo largo del proceso, se debe hacer el pre-procesado
-de datos.
-
+Data Preprocessinf Module
 Created on Fri Jan 6 12:07:01 2023
-
 @author: QV-137
 """
 
-# Dataset.csv from Part 1: Data preprocessing
-
-import numpy as np # For mathematical operations
-import matplotlib.pyplot as plt # For plotting information
-import pandas as pd # For managing large data collections
+import numpy as np
+import matplotlib.pyplot as plt 
+import pandas as pd
 
 # Importing dataset from ML Datasets folder
-dataset = pd.read_csv('C:/Users/1107473901/Documents/Proyectos Software/Python/Data Science and Statistics/Py Part 1 - Data pre-processing/Data1P.csv')
-# iloc = index + localization (It searches for the columns and rows)
-# rows in: fin, columns in:fin
+dataset = pd.read_csv('dataset.csv')
+# iloc = index + localization (It searches for the rows and columns)
 # We write [:, :] to make sure we include all rows and columns
 # We write [:-1].values to exclude just a final part of the table from the dataset
 # We write just [1,4] if we want specific rows & columns from the dataset
@@ -31,23 +21,16 @@ y = dataset.iloc[:, 3].values
 """
 The NaN data is replaced by the average or mean data of the column
 """
-"""
-CHECK LATER FOR SCIKITLEARN INSTALLATION ISSUE
-"""
-# from sklearn.preprocessing import Imputer
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values = np.nan, strategy = "mean")
 imputer = imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3])
 
-# DATOS CATEGÓRICOS
-# Codificar datos categóricos (en el dataset se tienen 3 países diferentes)
-# Pasar datos tipo string como Francia a números como 0
-
+# Categorical Data Encoding
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 
-# Encoding x
+# Encoding x_categorical_data
 labelencoder_x = LabelEncoder()
 x[:, 0] = labelencoder_x.fit_transform(x[:, 0])
 
@@ -57,20 +40,16 @@ ct = ColumnTransformer(
 )
 x = np.array(ct.fit_transform(x), dtype=np.float64)
 
-# Encoding y
+# Encoding y_categorical_data
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
 
-# DIVIDIR el dataset en conjunto de entrenamiento y conjunto de testing
+# Divide data into training and testing data sets
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
 
-# Escalado de variables
+# Variable scaling
 from sklearn.preprocessing import StandardScaler
 sc_x = StandardScaler()
 x_train = sc_x.fit_transform(x_train)
 x_test = sc_x.transform(x_test)
-
-
-
-
